903719330283ff9d0eb21624b332fcc24c764e15
Tomasz Smelcerz <tomasz.smelcerz@sap.com>
2026-01-17 03:48:49 +0100

    Add dependency to depstest-a

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Tomasz-Smelcerz-SAP/depstest-b
 
 go 1.25.5
+
+require github.com/Tomasz-Smelcerz-SAP/depstest-a v0.0.0-20260117023839-69b72815e6da
--- pkg/container/container.go
+++ pkg/container/container.go
@@ -1,6 +1,13 @@
 package container
 
+import (
+	"github.com/Tomasz-Smelcerz-SAP/depstest-a/pkg/component"
+)
+
 type Container struct {
-	elements []int
+	elements []component.Component
 }
 
+type ContainerSet struct {
+	containers []Container
+}
